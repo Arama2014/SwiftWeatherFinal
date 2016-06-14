abd1580adbe2592d6b6ae1e518de980ab24b3d08
AramaMara <ancientrome1>
2016-06-14 11:56:01 -0400

    first commit

---
+++ DailyWeatherTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  DailyWeatherTableViewCell.swift
+//  Stormy
+//
+//  Created by Arama Brown on 5/18/16.
+//  Copyright (c) 2016 Arama Brown. All rights reserved. 
+//
+
+import UIKit
+
+class DailyWeatherTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var dayLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by Arama Brown on 5/12/16.
+//  Copyright (c) 2016 Arama Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
